dfab2abca597e797c8105371cc1a5d03b1df4032
Izukill <luanloreto639@gmail.com>
2026-05-02 03:14:29 -0300

    fix: cors e axios pra conexao no deploy

--- api/src/main/java/org/Izuki/config/SecurityConfig.java
+++ api/src/main/java/org/Izuki/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://botboyia.vercel.app"));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD","PATCH"));
 
